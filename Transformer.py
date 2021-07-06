db6054bb24245140207f2222e0a00ff193b6fee2
Ali Almousawi <71436481+alihashemcs@users.noreply.github.com>
2021-07-06 03:32:32 +0300

    Seperated parseCSV and Transformer

--- Transformer.py
+++ Transformer.py
@@ -8,43 +8,16 @@ import csv
 from array import *
 from numpy import *
 import numpy as np
+import parseCSV
 
-# Import data from csv
-# then create numpy array from python list
-# then transform columns in numpy array
 
-#Gets name of csv file and puts data into python list
-# 400 rows, columns 1,2,4,5 (starting at index 0)
-def csvToPythonList(s):
-	w = ['']*4
-	v = [w]*400
-
-	with open(s) as dataCSV1:
-		csv_reader = csv.reader(dataCSV1, delimiter=',')
-		line_count = 0
-		count = 0
-		for row in csv_reader:
-			if(count!=400):
-				if line_count == 0:
-					line_count += 1
-				else:
-					v.insert(count,[row[1],row[2],row[4],row[5]])
-					line_count += 1
-					count += 1
-					v.remove(['', '', '', ''])
-		print(f'Processed {line_count-1} lines.')
-	return v
-
-#Python list to numpy array
-def pythonListToNumpyArray(l):
-	X = np.array(l,dtype=object)
-	return X
+# Transform columns in numpy array
 
 #Transform columns in numpy array
-def transformColsNumpyArray(a):
+def transformColsNumpyArray(a):	 
 	column_trans = ColumnTransformer(
-				[('source', OneHotEncoder(dtype='int'),[1])],
-		 remainder='passthrough')
+				[('source', OneHotEncoder(dtype='int'),[1,2])],
+     remainder='passthrough')
 
 	Y = column_trans.fit_transform(a)
 	return Y
--- parseCSV.py
+++ parseCSV.py
@@ -1,8 +1,55 @@
+import sys
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.feature_extraction.text import CountVectorizer
 import csv
 from array import *
 from numpy import *
 import numpy as np
 
+# Import data from csv
+# then create numpy array from python list
+
+#Gets name of csv file and puts data into python list
+# 400 rows, columns 1,2,4,5 (starting at index 0)
+def csvToPythonList(s):
+	w = ['']*4
+	v = [w]*400
+
+	with open(s) as dataCSV1:
+		csv_reader = csv.reader(dataCSV1, delimiter=',')
+		line_count = 0
+		count = 0
+		for row in csv_reader:
+			if(count!=400):
+				if line_count == 0:
+					line_count += 1
+				else:
+					v.insert(count,[row[1],row[2],row[4],row[5]])
+					line_count += 1
+					count += 1
+					v.remove(['', '', '', ''])
+		print(f'Processed {line_count-1} lines.')
+	return v
+
+#Python list to numpy array
+def pythonListToNumpyArray(l):
+	X = np.array(l,dtype=object)
+	return X
+
+def main():
+	x = str(sys.argv[1])
+	X = pythonListToNumpyArray(csvToPythonList(x))
+	print(X)
+	print("Testing the main() test client with command line arguments to test module.")
+	print(157)
+	print(x)
+
+if __name__ == '__main__' : main()
+
+"""
 z = [[]]
 w = ['']*4
 v = [w]*400
@@ -41,3 +88,4 @@ A = np.array([[1,2,3],[4,5,6],[7,8,9]])
 print(Y.dtype,Y.shape)
 print(Z.dtype,Z.shape)
 print(A.dtype,A.shape)
+"""
