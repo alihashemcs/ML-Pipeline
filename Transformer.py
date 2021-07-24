b9c1a6216bed2fa90b27b1e1deb26be8fc4ced81
Ali Almousawi <71436481+alihashemcs@users.noreply.github.com>
2021-07-24 17:04:46 +0300

    transformer working

--- Transformer.py
+++ Transformer.py
@@ -2,18 +2,21 @@ import sys
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import KBinsDiscretizer
+from sklearn.preprocessing import Normalizer
 from sklearn.feature_extraction.text import CountVectorizer
 import parseCSV
 
-# Transform columns in numpy array
-
 #Transform columns in numpy array
 def transformColsNumpyArray(a):	 
+	#discretize the time feature/column
+	#normalize the length feature/column
+	#encode sourceIP and protocol features 
 	column_trans = ColumnTransformer(
-				[('source', OneHotEncoder(dtype='int'),[1,2])],
-     remainder='passthrough')
-	
-	
+				[('time_disc', KBinsDiscretizer(n_bins=10, encode='onehot'), ['time']),
+				('source_protocol_enc', OneHotEncoder(dtype='int'), [1,2]),
+				('length_norm', Normalizer(), ['length'])],
+				remainder='drop')
 
 	Y = column_trans.fit_transform(a)
 	return Y
@@ -22,6 +25,7 @@ def main():
 	x = str(sys.argv[1])
 	y = parseCSV.csvToPythonList(x)
 	X = parseCSV.pythonListToNumpyArray(y)
+	X = parseCSV.numpyArrayToPandasDF(X)
 	print(X)
 	Y = transformColsNumpyArray(X)
 	print(Y)
