063f4e9924e6396eee5fb1af640d80faad34cc67
Ali Almousawi <71436481+alihashemcs@users.noreply.github.com>
2021-09-18 22:05:25 +0300

    Update GaussianNB

--- GaussianNaiveBayes.py
+++ GaussianNaiveBayes.py
@@ -14,38 +14,44 @@ def createGaussianNaiveBayesModel(X,y):
 def main():
 
 #Build Model
+	#Retieve + Transform input from csv file
 	x = str(sys.argv[1])
 	X = parseCSV.csvToPythonList(x)
 	X = parseCSV.pythonListToNumpyArray(X)
 	X = parseCSV.numpyArrayToPandasDF(X)
-	
-	#print(X)
 	X = Transformer.transformColsNumpyArray(X)
 	print("Input\n", X)
-	
+	#Create data labels array
 	y1 = np.zeros(2247,dtype=np.int64)
 	y2 = np.ones(1672,dtype=np.int64)
 	y = np.concatenate((y1,y2), axis=0)
 	print("Labels\n", y)
+	#Build model with data and labels
 	gnb = createGaussianNaiveBayesModel(X,y)
-	#print("Labels\n", kmeans.labels_)
-	#print("Cluster centers\n", kmeans.cluster_centers_)
-	"""
-    #Test model with test data (mirai-httpflooding-2-dec.csv)
+
+#Test Model using cross validation
+	result = cross_validate(gnb,X,y)
+	print(result)
+
+#Test model with test data (mirai-httpflooding-2-dec.csv)
 	z = str(sys.argv[2])
 	Z = parseCSV.csvToPythonList(z)
 	Z = parseCSV.pythonListToNumpyArray(Z)
 	Z = parseCSV.numpyArrayToPandasDF(Z)
-	
 	Z = Transformer.transformColsNumpyArray(Z)
-	print("Test data\n", Y)
-
-	print("Predictions for test data\n", gnb.predict(Z))
-	"""
-
-	result = cross_validate(gnb,X,y)
-	
-	print("Testing the main() test client with command line arguments to test module.")
-	print(result)
+	print("Test data\n", Z)
+	w = np.ones(3919,dtype=np.int64)
+	v=gnb.predict(Z)
+	print("Predictions for test data\n", v)
+	print("Score\n", gnb.score(Z,w))
+	ones=0
+	zeros=0
+	for i in v:
+		if i == 0:
+			zeros+=1
+		else:
+			ones+=1
+	print("Ones: \n", ones)
+	print("Zeros: \n", zeros)
 
 if __name__ == '__main__' : main()
