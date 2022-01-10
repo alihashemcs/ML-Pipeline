1a228675f6be3ae5954afe4ec44b95db9f1c2c04
Ali <ali@Alis-MacBook-Pro.local>
2022-01-10 10:02:44 +0300

    Changed file names yet again, dash '-' was causing an issue

---
+++ aIBMDPGaussianNB.py
@@ -0,0 +1,44 @@
+import sys
+from diffprivlib.models import GaussianNB
+from sklearn.model_selection import cross_validate
+import parseCSV
+import Transformer
+import numpy as np
+import pandas as pd
+import diffprivlib as ibmdp
+
+#Create a GaussianNB model
+def createDPGaussianNBModel(X,y):
+	dpgnb = GaussianNB()
+	dpgnb.fit(X,y)
+	return dpgnb
+
+def main():
+
+#Build Model
+	#Retieve + Transform input from csv file
+	x = str(sys.argv[1])
+	X = parseCSV.csvToPythonList(x)
+	X = parseCSV.pythonListToNumpyArray(X)
+	X = parseCSV.numpyArrayToPandasDF(X)
+	X = Transformer.transformColsNumpyArray(X)
+	print("Input\n", X)
+	#Create data labels array
+	y1 = np.zeros(2247,dtype=np.int64)
+	y2 = np.ones(1672,dtype=np.int64)
+	y = np.concatenate((y1,y2), axis=0)
+	print("Labels\n", y)
+	#Build model with data and labels
+	dpgnb = createDPGaussianNBModel(X,y)
+	
+#Test Model using cross validation
+	result = cross_validate(dpgnb,X,y)
+	test_score = result["test_score"]
+	avg = 0
+	for i in test_score:
+		avg += i
+	avg = avg / len(test_score)
+	print("CV Result: \n", result)
+	print("Average Test Score: \n", avg)
+
+if __name__ == '__main__' : main()
---
+++ aRandomForestClassifier.py
@@ -0,0 +1,42 @@
+import sys
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_validate
+import parseCSV
+import Transformer
+import numpy as np
+
+#Create a RandomForestClassifier model
+def createRandomForestClassifier(X,y):
+	clf = RandomForestClassifier(random_state=0)
+	#y = np.zeros(400,dtype=np.int64)
+	clf.fit(X,y)
+	return clf
+
+#print(clf.fit(X,y))
+#print(clf.predict(X))
+#print(clf.predict([[4, 5, 6], [14, 15, 16]]))
+
+def main():
+	x = str(sys.argv[1])
+	X = parseCSV.csvToPythonList(x)
+	X = parseCSV.pythonListToNumpyArray(X)
+	X = parseCSV.numpyArrayToPandasDF(X)
+	
+	#print(X)
+	X = Transformer.transformColsNumpyArray(X)
+	y = np.zeros(1000,dtype=np.int64)
+	print(X)
+	print(y)
+	
+	clf = createRandomForestClassifier(X,y)
+	#print(clf)
+
+	print(clf.predict(X))
+	
+	result = cross_validate(clf,X,y)
+	
+	print("Testing the main() test client with command line arguments to test module.")
+	print(result)
+
+
+if __name__ == '__main__' : main()
